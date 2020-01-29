a3afbb9c94de351b8a36502a6a555af73006f8fb
unknown <kshham@office.sungkyul.ac.kr>
2020-01-29 19:25:12 +0900

    Updates

---
+++ precache-manifest.6917693a7fecb331281758a4c34f2dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccd7c64ffcbb6ea8f3c9b5f0510aa35",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "6b14abd247bfc5e73c2f",
+    "url": "/movie_app_2019/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "fe7e2cab0773743a5557",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.2df6c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b14abd247bfc5e73c2f",
+    "url": "/movie_app_2019/static/js/main.6fa682ff.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
